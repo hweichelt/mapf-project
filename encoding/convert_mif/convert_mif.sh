22e8f3c23c10bea74efdca7031042d28a345fdee
Hannes Weichelt <hweichelt@uni-potsdam.de>
2023-07-06 17:26:45 +0200

    fixed path in convert_mif script

--- encoding/convert_mif/convert_mif.sh
+++ encoding/convert_mif/convert_mif.sh
@@ -3,5 +3,8 @@ if [ $# -eq 0 ]
 		echo "ERROR: no mif file specified"
 		exit 0
 fi
-clingo --out-atomf=%s. --out-ifs="\n" -q1 --outf=2 --warn=no-atom-undefined mif_to_asprilo.lp $1 | \
-python3 clingo_json_parser.py
+
+ENCSPDIR="$(dirname "$(realpath "$0")")"
+
+clingo --out-atomf=%s. --out-ifs="\n" -q1 --outf=2 --warn=no-atom-undefined $ENCSPDIR/mif_to_asprilo.lp $1 | \
+python3 $ENCSPDIR/clingo_json_parser.py
